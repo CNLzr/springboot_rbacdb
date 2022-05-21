f4ee0023e2b5159543d01b05a7a567671e4aace1
65137 <cnalzr@gmail.com>
2022-05-21 17:57:50 +0800

    权限管理模型

---
+++ src/main/java/com/lzr/config/ShiroConfig.java
@@ -0,0 +1,17 @@
+package com.lzr.config;
+
+import com.lzr.realms.MyRealm;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ShiroConfig implements WebMvcConfigurer {
+    @Bean
+    public DefaultWebSecurityManager securityManager(MyRealm myRealm){
+        DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
+        defaultWebSecurityManager.setRealm(myRealm);
+        return  defaultWebSecurityManager;
+    }
+}
---
+++ src/main/java/com/lzr/interceptor/JWTInterceptor.java
@@ -0,0 +1,14 @@
+package com.lzr.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JWTInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        return false;
+    }
+}
